d9b1ae25c72c825e830dcaab3596722916cbb06d
c9max <2290716817@qq.com>
2021-01-13 11:23:05 +0800

    注册新用户接口完成

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 // 创建服务器的实例对象
 const app = express();
 
+// 导入验证表单数据的包
+const joi = require("@hapi/joi");
+
 // 导入并配置 cors 中间件
 const cors = require("cors");
 app.use(cors());
@@ -10,9 +13,33 @@ app.use(cors());
 // 配置解析表单数据的中间件,注意:这个中间件只能解析`application/x-www-form-urlencoded`格式的表单数据
 app.use(express.urlencoded({ extended: false }));
 
+// 一定要在路由之前，封装res.cc函数
+app.use((req, res, next) => {
+    // status默认值为1，标识失败的情况
+    // err的值，可能是一个错误对象，也可能是一个错误的描述字符串
+    res.cc = function(err, status = 1) {
+        res.send({
+            status,
+            message: err instanceof Error ? err.message : err
+        })
+    }
+
+    next();
+})
+
 // 导入并使用用户路由模块
 const userRouter = require("./router/user");
 app.use("/api", userRouter);
+
+// 定义错误级别的中间件
+app.use((err, req, res, next) => {
+    // 验证失败导致的错误
+    if (err instanceof joi.ValidationError) {
+        return res.cc(err);
+    }
+    // 未知的错误
+    res.cc(err);
+})
 
 // 启动服务器
 app.listen(3007, () => {
--- router_handler/user.js
+++ router_handler/user.js
@@ -9,28 +9,41 @@ exports.regUser = (req, res) => {
     // 获取客户端提交到服务器的用户信息
     const userinfo = req.body;
     // 对表单中的数据，进行合法性的校验
-    if (!userinfo.username || !userinfo.password) {
-        return res.send({ status: 1, message: '用户名或密码不合法' });
-    }
+    // if (!userinfo.username || !userinfo.password) {
+    //     return res.send({ status: 1, message: '用户名或密码不合法' });
+    // }
     // 2. 检测用户名是否被占用
     // 定义SQL语句
     const sqlStr = 'select * from ev_users where username = ?';
     db.query(sqlStr, userinfo.username, (err, results) => {
         // 执行sql语句失败
         if (err) {
-            return res.send({ status: 1, message: err.message });
+            // return res.send({ status: 1, message: err.message });
+            return res.cc(err);
         }
         // 判断用户名是否被占用
         if (results.length > 0) { // 判断是否有数组对象
-            return res.send({ status: 1, message: '用户名被占用，请更换其他用户名' });
+            // return res.send({ status: 1, message: '用户名被占用，请更换其他用户名' });
+            return res.cc('用户名被占用，请更换其他用户名');
         }
+        // 3. 对密码进行加密处理
+        // 调用 bcrypt.hashSync(明文密码, 随机盐的长度) 对密码进行加密
+        // 随机盐的长度一般为10
+        userinfo.password = bcrypt.hashSync(userinfo.password, 10);
+        // 4. 插入新用户
+        // 定义插入新用户的SQL语句
+        const sql = 'insert into ev_users set ?';
+        // 调用 db.query() 执行SQL语句
+        db.query(sql, { username: userinfo.username, password: userinfo.password }, (err, results) => {
+            // 判断SQL语句是否执行成功
+            if (err) return res.cc(err);
+            // 判断影响行数是否为1
+            if (results.affectedRows !== 1) return res.cc("注册用户失败，请稍后再试！");
+            // 注册用户成功
+            // res.send({ status: 0, message: '注册成功！' });
+            res.cc('注册成功！', 0);
+        })
     });
-    console.log(userinfo);
-    // 3. 对密码进行加密处理
-    // 调用 bcrypt.hashSync(明文密码, 随机盐的长度) 对密码进行加密
-    // 随机盐的长度一般为10
-    userinfo.password = bcrypt.hashSync(userinfo.password, 10);
-    console.log(userinfo);
 }
 
 // 登录的处理函数
